288261b512dce8f8b0584c43a20d4355380897f7
danielbarion <danielbarionn@gmail.com>
2019-08-21 17:23:04 -0300

    Remove unnused toast component from project

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import './components/contact-card'
 import './components/field'
 import './components/filter'
 import './components/radio'
-import './components/toast'
 import './components/top-bar'
 
 /**
